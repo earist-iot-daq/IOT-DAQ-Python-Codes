4a52c0db82cc42e26427b850f0dad36491b29cfc
earist-iot-daq <79816617+earist-iot-daq@users.noreply.github.com>
2021-03-07 16:36:41 +0800

    Create extractingDataDelimeter.py

---
+++ extractingDataDelimeter.py
@@ -0,0 +1,19 @@
+"""
+String Manipulation
+v1.0 - Original file, delimiter ',', no code for compromising control characters.
+"""
+
+dataArray = []
+
+def extractData(extract):
+    count = 0
+    previousCount = 0
+ #   print('aasasaasa')
+    for x in extract:
+        count = count + 1
+        if (x == ','):
+            dataArray.append(extract[previousCount:count - 1])
+            previousCount = count
+        
+        if(count == len(extract)):
+            dataArray.append(extract[previousCount:len(extract)])
